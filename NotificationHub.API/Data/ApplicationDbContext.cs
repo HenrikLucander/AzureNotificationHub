ad4d88dbe14b1f2f3204933ac2c0791f163df887
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-14 14:37:04 +0200

    Added sqlite db

---
+++ NotificationHub.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationHub.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationHub.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Device> Devices { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(user =>
+            {
+                user
+                .HasKey(u => u.Username);
+            });
+
+            builder.Entity<Device>(device =>
+            {
+                device
+                .HasKey(d => new { d.Id, d.Username });
+
+                device
+                .HasOne(d => d.User)
+                .WithMany(u => u.Devices)
+                .HasForeignKey(d => d.Username)
+                .OnDelete(DeleteBehavior.Restrict);
+
+                device
+                .Property(d => d.Platform)
+                .IsRequired();
+
+                device
+                .Property(d => d.PnsToken)
+                .IsRequired();
+
+                device
+                .Property(d => d.RegistrationId)
+                .IsRequired();
+            });
+        }
+    }
+}

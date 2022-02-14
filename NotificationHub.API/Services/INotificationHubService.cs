25ebb22f8f60e9564bd3a04786032bd768f16af6
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-14 14:50:51 +0200

    Added notification hub service and sas token service for azure

---
+++ NotificationHub.API/Services/INotificationHubService.cs
@@ -0,0 +1,17 @@
+﻿using NotificationHub.SharedLib.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotificationHub.API.Services
+{
+    public interface INotificationHubService
+    {
+        Task<string> CreateRegistration(SignInUserDto signInDeviceDto);
+        Task<string> UpdateRegistration(SignInUserDto signInDeviceDto, string registrationId);
+        Task DeleteRegistration(string registrationId);
+        Task SendNotification(string message, string tag);
+    }
+}

b2bb3cde40e7d53e214b8e1b2c44eaab15b8b4ed
Henrik Lucander <henrik.lucander@tietoevry.com>
2022-02-14 14:15:56 +0200

    Added API models

---
+++ NotificationHub.API/Models/Device.cs
@@ -0,0 +1,15 @@
+﻿using NotificationHub.SharedLib.Models;
+
+namespace NotificationHub.API.Models
+{
+    public class Device
+    {
+        public Guid Id { get; set; }
+        public Platform Platform { get; set; }
+        public string PnsToken { get; set; }
+        public string RegistrationId { get; set; }
+
+        public string Username { get; set; }
+        public User User { get; set; }
+    }
+}
